825eb1759ebb1a4f6a819ae8b15ed57cf84b0acd
vinayreddyking <vinayreddyking@gmail.com>
2020-04-27 08:41:38 +0530

    Add Functionality to Checkbox check's and line Through to text using the bool ischecked.

--- lib/widgets/task_tile.dart
+++ lib/widgets/task_tile.dart
@@ -1,13 +1,43 @@
 import 'package:flutter/material.dart';
 
-class TaskTile extends StatelessWidget {
+class TaskTile extends StatefulWidget {
+  @override
+  _TaskTileState createState() => _TaskTileState();
+}
+
+class _TaskTileState extends State<TaskTile> {
+  bool isChecked = false;
+
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      title: Text('This is the Task'),
-      trailing: Checkbox(
-        value: false,
+      title: Text(
+        'This is the Task',
+        style: TextStyle(
+          decoration: isChecked ? TextDecoration.lineThrough : null,
+        ),
       ),
+      trailing: TaskCheckBox(
+        checkBoxState: isChecked,
+        toggleCheckBoxState: (bool checkBoxState) => setState(() {
+          isChecked = checkBoxState;
+        }),
+      ),
+    );
+  }
+}
+
+class TaskCheckBox extends StatelessWidget {
+  final bool checkBoxState;
+  final Function toggleCheckBoxState;
+  TaskCheckBox({this.checkBoxState, this.toggleCheckBoxState});
+
+  @override
+  Widget build(BuildContext context) {
+    return Checkbox(
+      activeColor: Colors.lightBlueAccent,
+      value: checkBoxState,
+      onChanged: toggleCheckBoxState,
     );
   }
 }
